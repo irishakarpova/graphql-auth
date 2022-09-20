642998646531297d5c7f0e95c6378f1cd6337d84
irina <irina.k@finterna.tech>
2022-09-20 18:17:11 -0500

    requestLink update

--- front-end/src/utils/authLink.tsx
+++ front-end/src/utils/authLink.tsx
@@ -1,32 +1,14 @@
 import { ApolloLink, Observable } from '@apollo/client';
 import { getAccessToken } from '../AccessToken';
 
-export const requestLink = new ApolloLink(
-  (operation, forward) =>
-    new Observable((observer) => {
-      let handle: any;
-      Promise.resolve(operation)
-        .then((operation) => {
-          const accessToken = getAccessToken();
-          if (accessToken) {
-            operation.setContext({
-              headers: {
-                authorization: `bearer ${accessToken}`,
-              },
-            });
-          }
-        })
-        .then(() => {
-          handle = forward(operation).subscribe({
-            next: observer.next.bind(observer),
-            error: observer.error.bind(observer),
-            complete: observer.complete.bind(observer),
-          });
-        })
-        .catch(observer.error.bind(observer));
-
-      return () => {
-        if (handle) handle.unsubscribe();
-      };
-    })
-);
+export const requestLink = new ApolloLink((operation, forward) => {
+  const accessToken = getAccessToken();
+  if (accessToken) {
+    operation.setContext({
+      headers: {
+        authorization: `bearer ${accessToken}`,
+      },
+    });
+  }
+  return forward(operation);
+});
